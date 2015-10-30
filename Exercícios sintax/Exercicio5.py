1781a4993e6f4adcef947a70c2ca6e9da6beedd6
rlindoso <rafael.lindoso@gmail.com>
2015-10-30 14:57:23 -0300

    issue #1 Código completo exercício 5

--- Exercícios sintax/Exercicio5.py
+++ Exercícios sintax/Exercicio5.py
@@ -1,17 +1,58 @@
-print('exercicio 5')
+print('**** IMPRENSADINHO ****\n')
 
-num = int(input('\ndigite seu numero no sorteio: '))
+valid = False
 
 from random import randint
+menor = 0;
+maior = 100;
+sorte = randint(menor+1,maior-1)
 
-sorte = randint(1,100)
-if sorte < num:
-    print('numero sorteado menor que seu numero')
-elif sorte > num:
-    print('numero sorteado maior que seu numero')
-else:
-    print('voce foi sorteado!')
+while not valid:
+    try:
+        qtdPessoas = int(input ('Digite o quantidade de pessoas \nque participarão do jogo: '))
+        if qtdPessoas > 0:
+            valid = True
+        else:
+            print ('quantidade deve ser positiva')
+    except:
+        print ( ' número incorreto ' )
 
-print('\n \n************* \n \n')
-print(sorte)
-print(num)
+array=[[0 for j in range(2)] for i in range(qtdPessoas)]
+
+for i in range(qtdPessoas):
+    nome = input ('Digite nome do %uº jogador: '%(i+1))
+    array[i] = [nome, 1]
+
+num = 0;
+idPessoa = 0;
+while (num < sorte) or (num > sorte):   
+    valid = False
+
+    print('está na vez de ' + array[idPessoa][0])
+    
+    while not valid:
+        try:
+            num = int (input ('Digite a um número entre %u e %u: ' %(menor, maior)))
+            if (num <= maior) and (num >= menor) and (num != menor) and (num != maior):
+                valid = True
+                if num < sorte:
+                    print('Número menor que o sorteado')
+                    menor = num
+                elif num > sorte:
+                    print('Número maior que o sorteado')
+                    maior = num
+                else:
+                    print('Acertou!\nO vencedor é %s com %u tentativas.' %(array[idPessoa][0], array[idPessoa][1]))
+                    maior = num
+
+                array[idPessoa][1] = array[idPessoa][1] + 1    
+                if idPessoa == qtdPessoas - 1:
+                    idPessoa = -1;
+                    
+                idPessoa = idPessoa + 1
+            else:
+                print ('Número deve estar entre %u e %u.' %(menor, maior))    
+        except:
+            print ( ' número incorreto ' )
+
+
